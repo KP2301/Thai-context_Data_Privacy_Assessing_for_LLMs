64fd069594a8faed14f48eb27d19848a1e5ce44d
Kittiwin Phannachet <kittiwinktb@gmail.com>
2026-01-18 01:47:15 +0700

    complete ceremonial round 2 for three models

--- attacks/DataExtraction/enron.py
+++ attacks/DataExtraction/enron.py
@@ -15,7 +15,7 @@ class EnronDataExtraction:
         
         self.data_path = data_path
 
-        self.context = load_jsonl(os.path.join(data_path, "context.jsonl"))
+        self.context = load_jsonl(os.path.join(data_path, "ceremonial_1_610.jsonl"))
         self.email2name_json = load_jsonl(os.path.join(data_path, "email2name.jsonl"))
         self.email2name = {}
         for item in self.email2name_json:
