3e9c9e7e00b61c828a010f09c0625d8462cd5f3d
Kittiwin Phannachet <kittiwinktb@gmail.com>
2026-02-13 21:36:32 +0700

    complete run ceremonial in dea scrub only name th 1- 300 for meeting

--- dea_with_metric.py
+++ dea_with_metric.py
@@ -45,7 +45,7 @@ if "GROQ_API_KEY" not in os.environ:
 # ====================================================================================================== #
 # Prepare data and prompts
 
-file_name = "semi_formal_1_610.jsonl" # edit file to run here (file must be at data/enron)
+file_name = "ceremonial_1_610.jsonl" # edit file to run here (file must be at data/enron)
 enron = EnronDataExtraction(data_path="data/enron", file_name=file_name)
 print(f"Now running : {file_name}")
 
